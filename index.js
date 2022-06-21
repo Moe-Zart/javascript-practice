c767e2e8c9ee5a5082f3d3b111a3e57f057a5214
Moe-Zart <mohamedzreika03@outlook.com>
2022-06-21 23:41:09 +1000

    challenges 1-10

---
+++ index.js
@@ -0,0 +1,83 @@
+/*Challenge 1
+//function that returns he sum of two arguments
+function addition (a,b){
+    return a+b;
+}
+console.log(addition(-3,-6));
+*/
+
+/*Challenge 2
+//function that converts hours into seconds
+
+function hoursIntoSeconds(hours){
+return hours*3600
+}
+
+console.log(hoursIntoSeconds(10))
+*/
+
+/*Challenge 3
+//calculate the perimeter of a rectangle
+
+function calcPerimeter(width, length) {
+  return width * 2 + length * 2;
+}
+console.log(calcPerimeter(20, 10));
+*/
+
+/*Challenge 4
+//calculate the area of a triangle
+
+function calcTriangleArea(base,height){
+return (base*height)/2
+}
+console.log(calcTriangleArea(10,10));
+*/
+
+/*Challenge 5
+//add 'Frontend' to the end of an inputted string
+function appendFrontend(x){
+    return x+"Frontend"
+}
+console.log(appendFrontend('Apple'));
+*/
+
+/*Challenge 6
+//return true if the sum of 2 numbers is greater than 100, otherwise false
+function sumGreaterThan100(x,y) {
+ return x+y > 100 ? 'true' : 'false';
+}
+console.log(sumGreaterThan100(20,10));
+*/
+
+/*Challenge 7
+//returns true if less than or equal to 0, otherwise return false
+function lessThanOrEqualToZero(x){
+return x<=0;
+}
+console.log(lessThanOrEqualToZero(-3));
+*/
+
+/*Challenge 8
+//create an opposite Boolean
+function oppositeBoolean(tf){
+return !tf;
+}
+console.log(oppositeBoolean(false));
+*/
+
+/*Challenge 9
+//return true if not 0, otherwise return false
+function isNotZero(x){
+return x!==0
+}
+console.log(isNotZero(0));
+*/
+
+/*Challenge 10
+//return the remainder
+function calcRemainder(x, y) {
+  return x % y;
+}
+console.log(calcRemainder(9, 2));
+*/
